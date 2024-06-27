2ac627e7a0c4b53ffbd3230693f3b30579b9c3c0
JLSed <seddd001@gmail.com>
2024-06-27 16:42:32 +0800

    some changes to main game ui

--- ui/GameGui.java
+++ ui/GameGui.java
@@ -30,7 +30,6 @@ public class GameGui extends JFrame {
 
     static Player player;
     static Scanner scanner;
-    int Choice = 0;
 
     public GameGui() {
         setTitle("Java RPG");
@@ -41,9 +40,6 @@ public class GameGui extends JFrame {
 
         // STARTING SCREEN
         StartingMenu();
-        if (Choice == 1) {
-            System.out.println("hello");
-        }
         // JTextArea StartingMenuScreen = new JTextArea("ghello :)");
         // StartingMenuScreen.setBackground(Color.BLACK);
         // add(StartingMenuScreen, BorderLayout.CENTER);
@@ -56,6 +52,8 @@ public class GameGui extends JFrame {
 
     public void StartingMenu() {
         JPanel StartingMenuPanel = new JPanel();
+        StartingMenuPanel.setLayout(null);
+        StartingMenuPanel.setBackground(Color.BLACK);
         String labels[][] = {{"CONTINUE"}, {"NEW"}, {"QUIT"}};
         KeyListener SMchangePanel = new KeyAdapter() {
             @Override
@@ -63,20 +61,16 @@ public class GameGui extends JFrame {
                 if (e.getKeyChar() == KeyEvent.VK_ENTER) {
                     String entered = ((JButton) e.getComponent()).getText();
                     if (entered.equals("CONTINUE")) {
-                        // System.out.println(b.getText());
-                        // Choice = 1;
-                        // remove(StartingMenuPanel);
-                        // MainGame();
-                        // revalidate();
-                        // repaint();
+                         remove(StartingMenuPanel);
+                         MainGame();
+                         revalidate();
+                         repaint();
                     System.out.println("C");
             
                     } else if (entered.equals("NEW")) {
-                        Choice = 2;
                         System.out.println("N");
             
                     } else if (entered.equals("QUIT")) {
-                        Choice = 3;
                         System.out.println("Q");
             
                     }
@@ -84,13 +78,11 @@ public class GameGui extends JFrame {
             }
         };
         GameButton SMbuttonPanel = new GameButton(3, 1, labels, SMchangePanel);
-        StartingMenuPanel.setLayout(null);
-        StartingMenuPanel.setBackground(Color.BLACK);
         SMbuttonPanel.setBounds(240, 200, 100, 100);
-        StartingMenuPanel.add(SMbuttonPanel);
         JLabel SMTitle = new JLabel("javaRP{G}");
         SMTitle.setFont(new Font("Arial", Font.BOLD, 50));
         SMTitle.setBounds(170, 10, 300, 200);
+        StartingMenuPanel.add(SMbuttonPanel);
         StartingMenuPanel.add(SMTitle);
         add(StartingMenuPanel, BorderLayout.CENTER);
 
@@ -107,21 +99,28 @@ public class GameGui extends JFrame {
     public void MainGame() {
         JPanel MainScreenContainer = new JPanel();
         MainScreenContainer.setLayout(new BorderLayout());
-        JTextArea MainScreen = new JTextArea("test test");
-        MainScreen.setForeground(Color.GREEN);
+        JTextArea MainScreen = new JTextArea("ASDASD");
+        //TODO: make scrollpane that will automatically scroll down with animation when text is not fit
         MainScreen.setBackground(Color.BLACK);
+        MainScreen.setForeground(Color.GREEN);
+        MainScreen.setText("asdasdadasdasda");
         MainScreenContainer.add(MainScreen, BorderLayout.CENTER);
 
         JTextField TestScreen = new JTextField("asdasd asdasd ");
-        TestScreen.setBackground(Color.WHITE);
-        TestScreen.setForeground(Color.BLACK);
+        TestScreen.setBackground(Color.BLACK);
+        TestScreen.setForeground(Color.GREEN);
         MainScreenContainer.setPreferredSize(new Dimension(200, 100));
         MainScreenContainer.add(TestScreen, BorderLayout.PAGE_END);
 
-        add(MainScreenContainer, BorderLayout.LINE_START);
+        add(MainScreenContainer, BorderLayout.CENTER);
+
+        JPanel RightPanel = new JPanel();
+        RightPanel.setPreferredSize(new Dimension(120, 200));
+        RightPanel.add(new JTextArea("TESTTT"), BorderLayout.LINE_START);
+        add(RightPanel, BorderLayout.LINE_END);
 
         JPanel BottomPanel = new JPanel();
-        BottomPanel.setPreferredSize(new Dimension(500, 200));
+        BottomPanel.setPreferredSize(new Dimension(500, 100));
         add(BottomPanel, BorderLayout.PAGE_END);
     }
 
