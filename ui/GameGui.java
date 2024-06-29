212cbdfb3b7814f72ddfd36c2f3b7cfca89b8e79
JLSed <seddd001@gmail.com>
2024-06-29 14:35:13 +0800

    added alignment for text in main screen

--- ui/GameGui.java
+++ ui/GameGui.java
@@ -2,6 +2,7 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -18,9 +19,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import player.Player;
 import script.Gf; // Game Functions
@@ -28,8 +34,9 @@ import script.Gd; // Game Displays
 
 public class GameGui extends JFrame {
 
-    static Player player;
     static Scanner scanner;
+    Player player;
+    JTextPane MainScreen;
 
     public GameGui() {
         setTitle("Java RPG");
@@ -54,25 +61,25 @@ public class GameGui extends JFrame {
         JPanel StartingMenuPanel = new JPanel();
         StartingMenuPanel.setLayout(null);
         StartingMenuPanel.setBackground(Color.BLACK);
-        String labels[][] = {{"CONTINUE"}, {"NEW"}, {"QUIT"}};
+        String labels[][] = { { "CONTINUE" }, { "NEW" }, { "QUIT" } };
         KeyListener SMchangePanel = new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 if (e.getKeyChar() == KeyEvent.VK_ENTER) {
                     String entered = ((JButton) e.getComponent()).getText();
                     if (entered.equals("CONTINUE")) {
-                         remove(StartingMenuPanel);
-                         MainGame();
-                         revalidate();
-                         repaint();
-                    System.out.println("C");
-            
+                        remove(StartingMenuPanel);
+                        MainGame();
+                        revalidate();
+                        repaint();
+                        System.out.println("C");
+
                     } else if (entered.equals("NEW")) {
                         System.out.println("N");
-            
+
                     } else if (entered.equals("QUIT")) {
                         System.out.println("Q");
-            
+
                     }
                 }
             }
@@ -99,16 +106,22 @@ public class GameGui extends JFrame {
     public void MainGame() {
         JPanel MainScreenContainer = new JPanel();
         MainScreenContainer.setLayout(new BorderLayout());
-        JTextArea MainScreen = new JTextArea("ASDASD");
-        //TODO: make scrollpane that will automatically scroll down with animation when text is not fit
+        MainScreen = AlignedText(1);
         MainScreen.setBackground(Color.BLACK);
         MainScreen.setForeground(Color.GREEN);
-        MainScreen.setText("asdasdadasdasda");
-        MainScreenContainer.add(MainScreen, BorderLayout.CENTER);
+        JScrollPane MainScreenScroll = new JScrollPane(MainScreen);
+        MainScreen.setText("TEST TES T  asdhgfksjhdg fjhsgd fkjhgsdkhjfgkjhsagfkhg sakjhd \n asdjfhlskjdhflkjs");
+        MainScreen.setText("─────────█▄██▄█─────────\r\n" + //
+                "█▄█▄█▄█▄█▐█┼██▌█▄█▄█▄█▄█\r\n" + //
+                "███┼█████▐████▌█████┼███\r\n" + //
+                "█████████▐████▌█████████\r\n" + //
+                "");
+        MainScreenContainer.add(MainScreenScroll, BorderLayout.CENTER);
 
-        JTextField TestScreen = new JTextField("asdasd asdasd ");
+        JTextField TestScreen = new JTextField();
         TestScreen.setBackground(Color.BLACK);
         TestScreen.setForeground(Color.GREEN);
+        TestScreen.setFont(new Font("Arial", Font.PLAIN, 20));
         MainScreenContainer.setPreferredSize(new Dimension(200, 100));
         MainScreenContainer.add(TestScreen, BorderLayout.PAGE_END);
 
@@ -122,6 +135,18 @@ public class GameGui extends JFrame {
         JPanel BottomPanel = new JPanel();
         BottomPanel.setPreferredSize(new Dimension(500, 100));
         add(BottomPanel, BorderLayout.PAGE_END);
+    }
+
+    private JTextPane AlignedText(int text_alignment) {
+        // 0 = ALIGN_LEFT
+        // 1 = ALIGN_CENTER
+        // 2 = ALIGN_RIGHT
+        JTextPane textpane = new JTextPane();
+        StyledDocument doc = textpane.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, text_alignment);
+        doc.setParagraphAttributes(0, doc.getLength(), center, false);
+        return textpane;
     }
 
     public static void main(String[] args) {
